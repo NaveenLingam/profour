2a8680047480dd29a28f5e2f8cefb3b3bfea0436
NaveenLingam <naveenlingam@10decoders.in>
2020-05-29 08:02:23 +0000

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://profour-app.profour.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://profour-app.profour.svc.cluster.local:3009';
+export const ADMINURL = 'http://profour-app.profour.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://profour-app.profour.svc.cluster.local:3008';
